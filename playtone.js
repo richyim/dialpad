212ab8c235ea8e53023b04c383dd5c77f9fe613f
Rich Yim <layim@cisco.com>
2024-05-11 17:19:24 +0800

    Update playtone.js

--- playtone.js
+++ playtone.js
@@ -1,22 +1,24 @@
-window.onload = function() {
-  // Create an audio context
+// Assuming this file is to remain mostly the same, just ensure that you export the playTone function if you're using modules.
+// Otherwise, make sure this script is included before script.js in your HTML.
+
+function playTone(frequency, duration) {
   const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+  const oscillator = audioContext.createOscillator();
+  const gainNode = audioContext.createGain();
 
-  // Function to play a note for a given frequency and duration
-  function playTone(frequency, duration) {
-    const oscillator = audioContext.createOscillator();
-    const gainNode = audioContext.createGain();
+  oscillator.type = 'sine';
+  oscillator.frequency.value = frequency;
 
-    oscillator.type = 'sine';
-    oscillator.frequency.value = frequency;
+  gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+  gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + 0.01);
+  gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + duration);
 
-    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
-    gainNode.gain.linearRampToValueAtTime(1, audioContext.currentTime + 0.01);
-    gainNode.gain.exponentialRampToValueAtTime(0.001, audioContext.currentTime + duration);
+  oscillator.connect(gainNode);
+  gainNode.connect(audioContext.destination);
 
-    oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
+  oscillator.start(audioContext.currentTime);
+  oscillator.stop(audioContext.currentTime + duration);
+}
 
-    oscillator.start(audioContext.currentTime);
-    oscillator.stop(audioContext.currentTime + duration);
-  }
+// Expose playTone to the window object if not using modules
+window.playTone = playTone;
